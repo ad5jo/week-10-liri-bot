10ec08e364c396fca56732ea3b265a2f62b05d79
Dave Durkee <ad5jo@yahoo.com>
2017-01-19 17:51:50 -0600

    add content. Make Heroku website

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ZN0qq0nNys1ErYHXyacNdAwV_3',
+  consumer_secret: 'UMRWCfVGxw5PkOE2l6FkXs2GsnM2Dd6F1p5AvciqpFR94JqMK_i',
+  access_token_key: '281530944-XfEMf4Ncljy0ywAB3yWVEB2qaTj6tdUrLZWE99b_S',
+  access_token_secret: '0dtrKLXXyUaK7cGYUaghzWi65eAfMoUfcqVBQspKyKZM_j',
+}
